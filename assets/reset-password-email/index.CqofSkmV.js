a174a94d1201ca446ef9b45f24a86a145db6523b
Patrick DePratti <patrick.depratti@okta.com>
2026-06-25 12:53:37 -0400

    Updates

--- assets/reset-password-email/index.CqofSkmV.js
+++
@@ -1,2 +0,0 @@
-import"../shared/react-vendor.Bslw2Lok.js";import"../shared/vendor.DvmdWBXV.js";
-//# sourceMappingURL=index.CqofSkmV.js.map
---
+++ assets/reset-password-success/index.DfagLrCE.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.Bslw2Lok.js";import"../shared/vendor.CFzOqtBb.js";
+//# sourceMappingURL=index.DfagLrCE.js.map
