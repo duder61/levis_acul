e80b43fa10dacc94a7071ed0f3a3a1a1604ad058
Patrick DePratti <patrick.depratti@okta.com>
2026-06-25 11:17:50 -0400

    Updates

--- assets/login-password/index.S4FJIrN6.js
+++
@@ -1,2 +0,0 @@
-import"../shared/react-vendor.Bslw2Lok.js";import"../shared/vendor.DvmdWBXV.js";import"../shared/common.CaZltDZ9.js";
-//# sourceMappingURL=index.S4FJIrN6.js.map
---
+++ assets/reset-password/index.CSDTC9AB.js
@@ -0,0 +1,2 @@
+import"../shared/react-vendor.Bslw2Lok.js";import"../shared/vendor.DvmdWBXV.js";import"../shared/common.DuRV-y_U.js";
+//# sourceMappingURL=index.CSDTC9AB.js.map
